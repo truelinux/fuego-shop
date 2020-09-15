7312021b8fb053197c55879c353c692c16e5a486
truelinux <omgk45@gmail.com>
2020-09-14 20:12:36 -0700

    test

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const storeProducts = [
     title: "Fuego Classic Hoodie",
     img: "img/classic-hoodie.png",
     price: 20,
-    company: "Angelo",
+    company: "Fuego",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
     inCart: false,
